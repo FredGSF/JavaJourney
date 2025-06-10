a690c595a4868b5d4492c1729435a938bcb36e47
Frederico Fernandes <frederico.garcia.2000@gmail.com>
2025-06-10 00:10:18 -0400

    study 2

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args){
+
+    int Num1 = 15;
+    int Num2 = 16;
+    int Num3 = Num1 + Num2;
     System.out.println("hello worldsS");
     System.out.println("this is a test");
+    System.out.println(345);
+    System.out.println(Num1+Num2);
+    System.out.println(Num3);
 }
 }
---
+++ calculator.java
@@ -0,0 +1,8 @@
+public class calculator {
+    public static void main(String[] args) {
+        int x, y;
+        char action;
+        System.err.println("Incerte the first number");
+        
+    }
+}
