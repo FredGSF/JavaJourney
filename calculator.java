8cfb7993d2d3c987a81faf9b093983cb744b415a
Frederico Fernandes <frederico.garcia.2000@gmail.com>
2025-06-12 12:47:15 -0400

    calculator done

--- calculator.java
+++ calculator.java
@@ -1,16 +1,41 @@
+import java.util.Scanner;
+
 public class calculator {
     public static void main(String[] args) {
-        int x, y;
-        x = 50;
-        y = 40;                 
-        char action;
-        System.out.println("First number"); 
-        System.out.println("Secound number"); 
-        System.out.println("Action to be done? (+, -, x, /)"); 
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(x + y);
-        System.out.println(x - y);
-        System.out.println(x / y);
-        System.out.println(x * y);
+        // Ask for first number:
+        System.out.println("First number");
+        double num1 = scanner.nextDouble();
+        // Ask for secound number:
+        System.out.println("Secound number");
+        double num2 = scanner.nextDouble();
+        // Ask for the operation
+        System.out.println("Operation to be done? (+, -, *, /)");
+        char opt = scanner.next().charAt(0);
+
+        double result;
+
+        switch (opt) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+
+            default:
+                System.out.println("Invalid operation!!!");
+
+                return;
+        }
+        System.out.println("Result " + result);
+
     }
 }
