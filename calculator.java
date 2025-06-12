bb299bf1da1de021df234d31affc58fe19cd4267
Frederico Fernandes <frederico.garcia.2000@gmail.com>
2025-06-12 01:00:34 -0400

    day 3

--- calculator.java
+++ calculator.java
@@ -1,8 +1,16 @@
 public class calculator {
     public static void main(String[] args) {
         int x, y;
+        x = 50;
+        y = 40;                 
         char action;
-        System.err.println("Incerte the first number");
-        
+        System.out.println("First number"); 
+        System.out.println("Secound number"); 
+        System.out.println("Action to be done? (+, -, x, /)"); 
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x / y);
+        System.out.println(x * y);
     }
 }
